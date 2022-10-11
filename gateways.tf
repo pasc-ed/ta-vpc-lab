13255fd8078dc26fdc477b3326aab22fe413344c
pascal.edouard@brambles.com <pascal.edouard@cloudreach.com>
2022-10-11 10:48:48 +0100

    resolve known error

--- gateways.tf
+++ gateways.tf
@@ -11,7 +11,7 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eaip.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_a.id
 
   tags = {
